050e8f9c7a434b2f967e4d389ff1d21041619830
Mateus Miranda <mateuusmiranda20@gmail.com>
2022-12-21 18:11:38 -0300

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,11 @@
+function mouseOver () {
+    document.getElementById("mova").innerHTML = " Dias de treino Segunda há Sexta.";
+    document.getElementById("move").innerHTML = " Dias de treino Segunda há Sexta.";
+    document.getElementById("mover").innerHTML = " Dias de treino. Segunda há Sexta.";
+}
+
+function mouseOut () {
+    document.getElementById("mova").innerHTML = " 2 vezes na semana Consulta Horários";
+    document.getElementById("move").innerHTML = " 3 vezes na semana Consulta Horários";
+    document.getElementById("mover").innerHTML = " 5 vezes na semana Consulta Horários";
+}
