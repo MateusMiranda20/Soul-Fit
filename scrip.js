ab089b06f42e2a2db4ab82a053d5ccc57b195023
Mateus Miranda <mateuusmiranda20@gmail.com>
2023-01-20 23:02:22 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -9,3 +9,11 @@ function mouseOut () {
     document.getElementById("move").innerHTML = " 3 vezes na semana Consulta Horários";
     document.getElementById("mover").innerHTML = " 5 vezes na semana Consulta Horários";
 }
+
+function mobileMenu(){
+    if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+    } else {
+     itens.style.display = 'block'
+    }  
+}
